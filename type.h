95c2fb540caa358da819b6bbdc62d09a4c2cbabf
Edward LEI <charming.lei72@qq.com>
2019-04-23 21:08:27 +0800

    all types should be defined here

---
+++ type.h
@@ -0,0 +1,24 @@
+/*
+  type.h
+
+  Copyright (c) 2017 Nikos Tasios
+  Copyright (C) 2019 by Edward LEI
+
+  This code is licensed under the MIT license
+*/
+
+
+#ifndef _OZ_TYPE_H_
+#define _OZ_TYPE_H_
+
+
+typedef unsigned int oz_uint_t;
+typedef unsigned char oz_uchar_t;
+
+typedef __m128  oz_sse4f_t;   /* vector of 4 floats */
+typedef __m128i oz_sse4i_t;
+
+typedef __m128d oz_sse2d;     /* vector of 2 doubles (sse2) */
+
+
+#endif
